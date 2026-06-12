c99587093a3d41bfcc7ef5222f94c7dcdcc7e34b
liciazhu <liciazhu@tencent.com>
2026-06-12 16:00:32 +0800

    mod browser.py
    Signed-off-by: liciazhu <liciazhu@tencent.com>

--- examples/browser-sandbox/browser.py
+++ examples/browser-sandbox/browser.py
@@ -7,8 +7,8 @@ from playwright.sync_api import sync_playwright
 from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv(dotenv_path=Path(__file__).with_name(".env"),override=False)
-# os.environ["NODE_EXTRA_CA_CERTS"] = "/root/.local/share/mkcert/rootCA.pem"
+load_dotenv(dotenv_path=Path(__file__).with_name(".env"), override=False)
+os.environ["NODE_TLS_REJECT_UNAUTHORIZED"] = "0"
 os.environ["NODE_NO_WARNINGS"] = "1"
 
 template_id = os.environ["CUBE_TEMPLATE_ID"]
@@ -16,14 +16,16 @@ template_id = os.environ["CUBE_TEMPLATE_ID"]
 sandbox = Sandbox.create(template=template_id)
 print(sandbox.get_info())
 
-cdp_url = f"https://{sandbox.get_host(9000)}/cdp?"
-# 使用playwright通过cdp_url来操作浏览器
-from playwright.sync_api import sync_playwright
+host = sandbox.get_host(9000)
+cdp_url = f"https://{host}/cdp/"
+print(f"[*] CDP URL: {cdp_url}")
+
 with sync_playwright() as playwright:
-    browser = playwright.chromium.connect_over_cdp(
-        cdp_url,
-    )
+    browser = playwright.chromium.connect_over_cdp(cdp_url)
     context = browser.new_context(ignore_https_errors=True)
     page = context.new_page()
     page.goto("http://www.tencent.com", wait_until="domcontentloaded")
     print(page.title())
+
+sandbox.kill()
+print("[+] Done.")
